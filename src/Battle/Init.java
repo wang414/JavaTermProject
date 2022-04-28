a4ebd2f2bdac33defaaecd839de7a54cf1d0033d
RogersIV144 <2697541206@qq.com>
2022-04-28 21:29:17 +0800

    98135 进行的变更

--- src/Battle/Init.java
+++ src/Battle/Init.java
@@ -1,4 +1,5 @@
 package Battle;
+import javax.swing.*;
 
 public class Init {
 
--- src/Battle/MainLoop.java
+++ src/Battle/MainLoop.java
@@ -31,8 +31,20 @@ public class MainLoop {
     static String bgImg = "";
     public MainLoop(JFrame windows)
     {
+        /**
+         * should be disposed before calling.
+         *
+         * @Paramater main windows
+         */
         //初始化:init
-
+        window = windows;
+        Init();
+        windows.dispose();
+        windows.setLayout(null);
+        initMenu();
+        initBattle();
+        battlePanel = new Render();
+        windows.getLayeredPane().add(battlePanel);
         //战斗主循环:
         //渲染阶段:render
         rending = new Timer(60, (ActionEvent e)->
@@ -45,5 +57,47 @@ public class MainLoop {
         //植物,铲子的拖动,暂停,阳光拾取
 
         //结束
+    }
+    private void initMenu(){
+    }
+    private void initBattle(){
+
+    }
+    class Render extends JLayeredPane
+    {
+        @Override
+        protected void paintComponent(Graphics g)
+        {
+            //绘制背景
+            Image bg = curLevel.Background;
+            g.drawImage(bg, 0, 0, null);
+
+            //绘制功能栏+铲子+暂停
+
+            //绘制植物+小车
+            for(int i = 0;i < plants.size();i++)
+            {
+                g.drawImage(plants.get(i).img, plants.get(i).x, 0, null);
+            }
+
+            //绘制僵尸
+            for(int i = 0;i < zombies.size();i++)
+            {
+                g.drawImage(zombies.get(i).img, 0, 0, null);
+            }
+
+            //绘制子弹
+
+            //绘制阳光
+        }
+        //渲染阶段:
+        //算时间差, 开始渲染: 清空画布,从头开始画,遍历所有items:
+        //地图,植物,僵尸,阳光,小车,选择栏,铲子,暂停,进度条,光标
+    }
+    private void Init(){
+        //导入关卡信息
+        //渲染地图与出没僵尸
+        //选植物
+        //开始战斗
     }
 }
